d71a383aba912697f80342ce82bf5ade2164c973
chiperoo <chiperoo101@gmail.com>
2017-04-18 12:19:20 -0700

    Unit test for non-jsonapi RESTful call

--- src/test/java/dream/mystic/MysticControllerTest.java
+++ src/test/java/dream/mystic/MysticControllerTest.java
@@ -1,26 +1,16 @@
 package dream.mystic;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Optional;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.mock.http.MockHttpOutputMessage;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -28,9 +18,7 @@ import org.springframework.web.context.WebApplicationContext;
 
 import dream.mystic.domain.Customer;
 import dream.mystic.domain.Trip;
-import dream.mystic.domain.ActivityLog;
 import dream.mystic.repository.CustomerRepository;
-import dream.mystic.repository.ActivityLogRepository;
 import dream.mystic.repository.TripRepository;
 
 @RunWith(SpringRunner.class)
@@ -38,21 +26,13 @@ import dream.mystic.repository.TripRepository;
 @WebAppConfiguration
 public class MysticControllerTest {
 
-	private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
-            MediaType.APPLICATION_JSON.getSubtype(),
-            Charset.forName("utf8"));
-	
     private MockMvc mockMvc;
-
-    private HttpMessageConverter mappingJackson2HttpMessageConverter;
-    
+  
     private String name = "batman";
 
     private Customer customer;
     
     private Trip trip;
-
-    private List<ActivityLog> activityLogList = new ArrayList<>();
 
     @Autowired
     private CustomerRepository customerRepository;
@@ -61,56 +41,50 @@ public class MysticControllerTest {
     private TripRepository tripRepository;
     
     @Autowired
-    private ActivityLogRepository activityLogRepository;
-    
-    @Autowired
     private WebApplicationContext webApplicationContext;
-
-    @Autowired
-    void setConverters(HttpMessageConverter<?>[] converters) {
-
-        this.mappingJackson2HttpMessageConverter = Arrays.asList(converters).stream()
-            .filter(hmc -> hmc instanceof MappingJackson2HttpMessageConverter)
-            .findAny()
-            .orElse(null);
-
-        assertNotNull("the JSON message converter must not be null",
-                this.mappingJackson2HttpMessageConverter);
-    }
 	
     @Before
     public void setup() throws Exception {
         this.mockMvc = webAppContextSetup(webApplicationContext).build();
 
-        // avoids foreign key constraints in this order
-        this.activityLogRepository.deleteAllInBatch();
-        this.tripRepository.deleteAllInBatch();
-        this.customerRepository.deleteAllInBatch();
-
         this.customer = customerRepository.save(new Customer(name, name + "@user.email"));
         this.trip = tripRepository.save(new Trip("description"));
-        
-//        this.tripDetailList.add(activityLogRepository.save(new ActivityLog(this.customer, this.trip, "Entry 1")));
-//        this.tripDetailList.add(activityLogRepository.save(new ActivityLog(this.customer, this.trip, "Entry 2")));
     }
     
-//    @Test
-//    public void readSingleTripDetail() throws Exception {
-//        mockMvc.perform(get("/manage/"
-//                + this.tripDetailList.get(0).getId()))
-//                .andExpect(status().isOk())
-//                .andExpect(content().contentType(contentType))
-//                .andExpect(jsonPath("$.id", is(this.tripDetailList.get(0).getId().intValue())))
-//                .andExpect(jsonPath("$.sequence", is(this.tripDetailList.get(0).getSequence())))
-//                .andExpect(jsonPath("$.details", is("Step 1")));
-//    }
+    @Test
+    public void signUpCustomerForNewTrip() throws Exception {
+        // sign up for new trip via RESTful call
+    	mockMvc.perform(post("/mystic/"
+                + this.customer.getId() + "/addTrip/"  
+                + this.trip.getId()));
+        
+    	// check database if customer and trip records reflect the new signup
+        Optional<Customer> customerTest = customerRepository.findById(this.customer.getId());
+        Customer customerCopy = customerTest.get();
+        
+        Optional<Trip> tripTest = tripRepository.findById(this.trip.getId());
+        Trip tripCopy = tripTest.get();
+        
+        Assert.assertEquals(1, customerCopy.getTrips().size());
+        Assert.assertEquals(1, tripCopy.getCustomers().size());
+    }
     
     
-    
-    protected String json(Object o) throws IOException {
-        MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
-        this.mappingJackson2HttpMessageConverter.write(
-                o, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
-        return mockHttpOutputMessage.getBodyAsString();
+    @Test
+    public void removeCustomerFromNewTrip() throws Exception {
+        // remove trip from customer via RESTful call
+    	mockMvc.perform(post("/mystic/"
+                + this.customer.getId() + "/removeTrip/"  
+                + this.trip.getId()));
+        
+    	// check database if customer and trip records reflect the removal
+        Optional<Customer> customerTest = customerRepository.findById(this.customer.getId());
+        Customer customerCopy = customerTest.get();
+        
+        Optional<Trip> tripTest = tripRepository.findById(this.trip.getId());
+        Trip tripCopy = tripTest.get();
+        
+        Assert.assertEquals(0, customerCopy.getTrips().size());
+        Assert.assertEquals(0, tripCopy.getCustomers().size());
     }
 }
